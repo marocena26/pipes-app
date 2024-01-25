4ebe29b5703844d5f4eca3ab881423b0a69a7bd4
Elena Arocena <elena.arocena@innoway.es>
2024-01-25 11:38:15 +0100

    feat(products): add new structure for uncommon products

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -4,56 +4,62 @@ import { Observable, interval, tap } from 'rxjs';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.scss']
+  styleUrls: ['./uncommon-page.component.scss'],
 })
 export class UncommonPageComponent {
-// i18n Select
-public name: string = 'Fernando';
-public gender: 'male'|'female' = 'male';
-public invitationMap = {
-  male: 'invitarlo',
-  female: 'invitarla'
-}
+  // i18n Select
+  public name: string = 'Fernando';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
-changeClient():void {
-  this.name = 'Melissa';
-  this.gender = 'female';
-}
+  changeClient(): void {
+    this.name = 'Melissa';
+    this.gender = 'female';
+  }
 
-// i18nPlural
-public clients: string[] = ['Maria','Pedro','Fernando', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
-public clientsMap = {
-  '=0': 'no tenemos ningún cliente esperando.',
-  '=1': 'tenemos un cliente esperando.',
-  '=2': 'tenemos 2 personas esperando.',
-  'other': 'tenemos # clientes esperando.',
-}
+  // i18nPlural
+  public clients: string[] = [
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melissa',
+    'Natalia',
+  ];
+  public clientsMap = {
+    '=0': 'have no customers waiting.',
+    '=1': 'have 1 customer waiting',
+    '=2': 'have 2 people waiting.',
+    other: 'have # clients waiting.',
+  };
 
-deleteClient(): void {
-  this.clients.shift();
-}
+  deleteClient(): void {
+    this.clients.shift();
+  }
 
-// KeyValue Pipe
-public person = {
-  name: 'Fernando',
-  age: 36,
-  address: 'Ottawa, Canada',
-}
+  // KeyValue Pipe
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada',
+  };
 
-// Async Pipe
-public myObservableTimer: Observable<number> = interval(2000).pipe(
-  tap( value => console.log('tap:', value ) ),
-);
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap((value) => console.log('tap:', value))
+  );
 
-public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
-  setTimeout(() => {
-    resolve( 'Tenemos data en la promesa.' );
-    console.log( 'Tenemos data en la promesa.' );
-    this.person.name = 'Otro nombre'
-  }, 3500);
-})
-
-
-
-
+  public promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('We have data on the promise');
+      console.log('We have data on the promise');
+      this.person.name = 'Melissa';
+      this.person.age = 26;
+      this.person.address = 'Madrid, Spain';
+    }, 3500);
+  });
 }
